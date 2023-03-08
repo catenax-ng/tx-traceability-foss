55abbfeb078bd10e275b011256b2133c89f6d2b2
Laszlo Graf <laszlo.graf@msg.group>
2023-03-08 19:37:54 +0200

    eclipse-dash

--- .github/scripts/eclipse_dash.sh
+++ .github/scripts/eclipse_dash.sh
@@ -1,3 +1,3 @@
 java -jar org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar yarn.lock -project automotive.tractusx -summary DASH_SUMMARY
-pwd && ls -la DASH_SUMMARY
+pwd && ls -la
 grep -E '(restricted, #)|(restricted$)' DASH_SUMMARY | if test $(wc -l) -gt 0; then exit 1; fi
