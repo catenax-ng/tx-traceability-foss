ce68452d5856bbc62a76806857c685e81f001faa
Darek Skrzypczak <darek.skrzypczak@partner.doubleslash.de>
2023-03-29 10:37:37 +0200

    fix: TRACEFOSS-1285 Move FE and BE DEPENDENCIES file into root with the suffix

--- frontend/scripts/eclipse_dash.sh
+++ frontend/scripts/eclipse_dash.sh
@@ -1,2 +1,2 @@
-java -jar ../org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar yarn.lock -project automotive.tractusx -summary DEPENDENCIES
-grep -E '(restricted, #)|(restricted$)' DEPENDENCIES | if test $(wc -l) -gt 0; then exit 1; fi
+java -jar ../org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar yarn.lock -project automotive.tractusx -summary ../DEPENDENCIES_FRONTEND
+grep -E '(restricted, #)|(restricted$)' ../DEPENDENCIES_FRONTEND | if test $(wc -l) -gt 0; then exit 1; fi
