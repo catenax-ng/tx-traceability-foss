f8c2c16881f10f26a14dfb46595bf41a3478740e
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-04-19 11:40:37 +0200

    chore: TRACEFOSS-617 merged main.

---
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/infrastructure/jpa/bpn_edc/BpnEdcRecord.java
@@ -0,0 +1,40 @@
+/********************************************************************************
+ * Copyright (c) 2022, 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ * Copyright (c) 2022, 2023 ZF Friedrichshafen AG
+ * Copyright (c) 2022, 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+package org.eclipse.tractusx.traceability.infrastructure.jpa.bpn_edc;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record BpnEdcRecord(
+    @Size(min = 15, max = 255, message = "BPN should have at least 15 characters and at most 255 characters")
+    @ApiModelProperty(example = "The BPN")
+    String bpn,
+
+    @Size(min = 1, max = 100, message = "Specify at least 1 and at most 100 EDC URLs")
+    @ApiModelProperty(example = "[\"https://trace-x-test-edc.dev.demo.catena-x.net/a2\"]")
+    List<String> urls
+) {
+}
