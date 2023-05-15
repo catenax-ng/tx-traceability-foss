6cfc5ef3f12c6ae52a0e63a0a67dc4474fdce2e2
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-05-15 08:36:49 +0200

    feature: TRACEFOSS-1111 added loging on contract offer filter

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/infrastructure/edc/blackbox/EdcService.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/infrastructure/edc/blackbox/EdcService.java
@@ -81,11 +81,13 @@ public class EdcService {
         log.info(":::: Find Notification contract method[findNotificationContractOffer] total catalog ::{}", catalog.getContractOffers().size());
 
         if (isInitialNotification) {
+            log.info("Is receive notification filter");
             return catalog.getContractOffers().stream()
                     .filter(it -> it.getAsset().isQualityInvestigationReceive())
                     .filter(this::hasTracePolicy)
                     .findAny();
         } else {
+            log.info("Is update notification filter");
             return catalog.getContractOffers().stream()
                     .filter(it -> it.getAsset().isQualityInvestigationUpdate())
                     .filter(this::hasTracePolicy)
@@ -94,6 +96,9 @@ public class EdcService {
     }
 
     private boolean hasTracePolicy(ContractOffer contractOffer) {
+        if (contractOffer.getPolicy() != null) {
+            log.info(":::: ContractOffer {} hasTracePolicy {}", contractOffer, contractOffer.getPolicy().hasTracePolicy());
+        }
         return contractOffer.getPolicy() != null && contractOffer.getPolicy().hasTracePolicy();
     }
 
