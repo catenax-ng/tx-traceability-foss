b1727a75bc648cb7d72d1b02f6f5e2833c6e117e
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2023-05-15 15:44:27 +0200

    chore: TRACEFOSS-1376 code smells

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/infrastructure/adapters/jpa/metrics/registrylookup/RegistryLookupMetricEntity.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/infrastructure/adapters/jpa/metrics/registrylookup/RegistryLookupMetricEntity.java
@@ -21,20 +21,18 @@
 
 package org.eclipse.tractusx.traceability.assets.infrastructure.adapters.jpa.metrics.registrylookup;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.eclipse.tractusx.traceability.assets.infrastructure.adapters.metrics.RegistryLookupMetric;
-import org.eclipse.tractusx.traceability.assets.infrastructure.adapters.metrics.RegistryLookupStatus;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.eclipse.tractusx.traceability.assets.infrastructure.adapters.metrics.RegistryLookupStatus;
+
 import java.time.Instant;
 
 @Entity
