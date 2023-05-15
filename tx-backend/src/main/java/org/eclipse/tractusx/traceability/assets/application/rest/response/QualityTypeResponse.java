e9f747165d8bfd048d7d2eb90c063d07cf3bd5b4
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2023-05-15 15:34:18 +0200

    chore: TRACEFOSS-1376 After code review

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/application/rest/response/QualityTypeResponse.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/application/rest/response/QualityTypeResponse.java
@@ -1,7 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2022, 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
- * Copyright (c) 2022, 2023 ZF Friedrichshafen AG
- * Copyright (c) 2022, 2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -18,7 +16,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
-
 
 package org.eclipse.tractusx.traceability.assets.application.rest.response;
 
