011ebcf918bf5e448713e9c8237f2e00d02456bb
Martin Maul <martin.maul@doubleslash.de>
2023-07-10 17:18:11 +0200

    chore(trg):[TRACEFOSS-1967] added about card component and workflow to provide about info

--- frontend/src/app/modules/page/about/presentation/about.component.ts
+++ frontend/src/app/modules/page/about/presentation/about.component.ts
@@ -19,11 +19,41 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  @Input() name: string;
+  @Input() repositoryPath: string;
+  @Input() license: string;
+  @Input() licensePath: string;
+  @Input() noticePath: string;
+  @Input() sourcePath: string;
+  @Input() commitId: string;
+
+  constructor(private http: HttpClient) {
+    this.name = "Traceability-foss";
+    this.repositoryPath = "https://github.com/catenax-ng/tx-traceability-foss";
+    this.license = "Apache-2.0";
+    this.licensePath = "https://github.com/catenax-ng/tx-traceability-foss/blob/main/LICENSE";
+    this.noticePath = "https://github.com/catenax-ng/tx-traceability-foss/blob/main/NOTICE.md";
+    this.fetchAppInfo();
+
+  }
+
+   openLink(url: string) {
+    window.open(url, '_blank')
+  }
+
+   fetchAppInfo() {
+    this.http.get<any>('/assets/aboutInfo.json').subscribe(data => {
+      this.sourcePath = data.sourcePath;
+      this.commitId = data.commitId;
+    })
+  }
+}
